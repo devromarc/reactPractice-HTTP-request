94562d5e2938c0ff2d3db8d2bb9e6d7efb5d0053
devromarc <69194960+devromarc@users.noreply.github.com>
2025-04-09 14:45:17 +0800

    Getting the data from BE using axios and useEffect

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,33 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+import axios from "axios";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setavailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function getPlaces() {
+      try {
+        const response = await axios.get("http://localhost:3000/places");
+        return response.data.places;
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        return [];
+      }
+    }
+
+    (async () => {
+      // Immediately invoked async function expression (IIFE)
+      const places = await getPlaces();
+      setavailablePlaces(places);
+    })();
+  }, []);
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
+      onSelectPlace={availablePlaces}
     />
   );
 }
